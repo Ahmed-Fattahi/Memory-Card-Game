13451265d6f75149ab966064902cda1ad9b636b2
Ahmed-Fattahi <fattahi.ahmed1@gmail.com>
2024-05-13 11:44:39 +0100

    Add the logic code for images comparison
    - Add numbers as a parametters in the onclick on HTML file
    - Update rotate function in Js file for comparing the images
    
    Fix #4

--- Main.js
+++ Main.js
@@ -1,8 +1,8 @@
 
 table = ['images/anapec.png','images/aups.png','images/atlantis.png','images/ecl.png','images/planet.png','images/sage.png']
-i=0
+i=0;
 let counter=[];
-
+let images=[];
 let click = -1;
 
 while(true){
@@ -11,16 +11,34 @@ while(true){
         document.querySelector("#image"+random+" .back").style.backgroundImage =('url("'+ table[i] +'")'); 
         counter.push(random);
 
+        images[random]=table[i];
+
         i++;
         if(i==6) i=0;
         if(counter.length == 12) break;
     }
 }
+
 function rotate(id){
-    document.getElementById(id).classList.add('rotate');
+    document.getElementById("image"+id).classList.add('rotate');
     click = click*(-1);
+
+    if(click==1){
+        firstId = id ;
+    }
+    if(click==-1){
+        if(firstId==id){
+            click = click *(-1);
+        }
+        else{
+            secondId = id ;
+    
+            if(images[firstId]==images[secondId]){
+                console.log("---------------------is the same---------------------")
+            }
+            else{
+                console.log("is not the same")
+            }
+        }
+    }
 }
-
-
-
-
